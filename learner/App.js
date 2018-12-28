7b382acef39860c47a61ccd00e9c04bf51f8bb44
Eric Laursen <ericjlaursen@gmail.com>
2018-12-27 19:46:38 -0500

    Includes state

--- learner/App.js
+++ learner/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 class Greeting extends Component {
   render() {
@@ -12,16 +12,53 @@ class Greeting extends Component {
 }
 
 const nameArr = ['Rexxar', 'Jaina', 'Valeera'];
+const blinkingTextArr = ['first binking text', 'second text', 'third']
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
 
 const greetings = nameArr.map(n => {
   return <Greeting key={n} name={n} />
 })
 
+const blinkers = blinkingTextArr.map((t, ind) => {
+  return <Blink key={ind} text={t}/>
+})
+
 export default class LotsOfGreetings extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isShowing: true
+    }
+  }
+
   render() {
     return (
       <View style={{alignItems: 'center', marginTop: 100}}>
         {greetings}
+        {blinkers}
       </View>
     );
   }
