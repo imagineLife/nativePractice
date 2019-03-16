6296221947d62c6d5fa2fb55a22cf571110c61c7
Eric Laursen <ericjlaursen@gmail.com>
2019-03-16 07:54:08 -0400

    div with p

--- GithubFetcher/App.js
+++ GithubFetcher/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import {
+  View, 
+  Text
+} from 'react-native'
 
 export default class App extends Component {
   render() {
     return (
-
+      <View>
+        <Text>This is like a p tag in a div</Text>
+      </View>
     );
   }
 }
