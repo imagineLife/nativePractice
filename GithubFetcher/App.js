94de3920eec5b7e7b328d144eb1b67254a789315
Eric Laursen <ericjlaursen@gmail.com>
2019-03-18 21:09:46 -0400

    Placeholder for FlatList

--- GithubFetcher/App.js
+++ GithubFetcher/App.js
@@ -1,138 +1,79 @@
 import React, {Component} from 'react';
+
 import {
-  View, 
+  View,
   Text,
   StyleSheet,
-  TextInput, //a text box
-  Platform // device-specific
+  TextInput,
+  ScrollView,
+  FlatList
 } from 'react-native'
 
-import Header from './src/components/Header';
-import List from './src/components/List';
+import Header from 'components/Header'
 
-const places = [
-  {
-    name: 'Example Restaurant 1',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 2',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 3',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 21',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 22',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 23',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 31',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 32',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 33',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 41',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 42',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 43',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 51',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 52',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 53',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 61',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 62',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 63',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 71',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 72',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 73',
-    addr: '123 North st'
-  },
-  {
-    name: 'Example Restaurant 81',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 82',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 83',
-    addr: '123 North st'
-  }
+import RestaurantRow from 'components/RestaurantRow'
+
+const restaurants = [
+  {name: 'React Cafe', address: '123 Anywhere St'},
+  {name: 'Fancy Restaurant', address: '799 Main St'},
+  {name: 'Taco Place', address: '550 Maple Rd'},
+  {name: "Tony's Diner", address: '4101 College St'},
+  {name: 'Pasta Central', address: '706 Harper St'},
+  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
+  {name: 'Pizza Express', address: '1049 Bird St'},
+  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
+  {name: 'Maroon Deli', address: '1082 Stuart St'},
+  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
+  {name: 'Dumpling House', address: '747 Kelly Dr'},
+  {name: 'Hot Chicken', address: '1816 Olive St'},
+  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
+  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
+  {name: "Bobby's Burgers", address: '4152 Berkley St'},
+  {name: 'Turnpike Diner', address: '4571 Central Ave'},
+  {name: 'Bombay Express', address: '65 Queens Lane'},
+  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
+  {name: "King's Garden", address: '2935 Victoria Ct'},
+  {name: 'Salads and More', address: '2454 Preston St'},
 ]
 
 export default class App extends Component {
+
   state = {
-    searchVal: null
+    search: null
   }
+
   render() {
 
     return (
-      <View
-        style={{flex:1}}>
+      <View style={{
+        flex: 1
+      }}>
 
-        <Header style={styles.title} />
+        <Header />
 
         <TextInput 
           style={styles.input} 
-          placeholder={'Live Search'}
-          onChangeText={textVal => {
-            this.setState({searchVal: textVal})
+          placeholder="Live Search"
+          onChangeText={text => {
+            this.setState({ search: text })
           }}
-          value={this.state.searchVal} />
+          value={this.state.search}
+        />
 
-        <List 
-          data={places}
-          searchVal={this.state.searchVal}
-          styles={styles}/>
+        <FlatList
+          data = {
+            restaurants.filter(place => {
+              return !this.state.search ||
+                place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+            })
+          }
+          renderItem={({ item, index }) => 
+            <RestaurantRow place={item} index={index} />
+          }
+          keyExtractor={item => item.name}
+          initialNumToRender={16}
+          ListHeaderComponent={<View style={{height: 30}} />}
+        />
 
       </View>
     );
@@ -140,25 +81,7 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  title: {
-    padding: 40,
-    fontSize: 30,
-    textAlign: 'center',
-    fontWeight: '300',
-    color: 'steelblue'
-  },
-  row: { flexDirection: 'row'},
-  edges: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  stats: {
-    flexDirection: 'column',
-    flex: 8
-  },
   input: {
-    marginBottom: 30,
     padding: 10,
     paddingHorizontal: 20,
     fontSize: 16,
