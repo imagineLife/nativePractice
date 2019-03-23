6d91d4f5104da5e6662925c3c6a32848df6d7ad3
Eric Laursen <ericjlaursen@gmail.com>
2019-03-23 16:32:54 -0400

    resets layout, loads fine!

--- GithubFetcher/App.js
+++ GithubFetcher/App.js
@@ -7,18 +7,26 @@ import {
 } from 'react-native'
 
 const places = [
-  {
-    name: 'Example Restaurant 1',
-    addr: '123 Main st'
-  },
-  {
-    name: 'Example Restaurant 2',
-    addr: '123 South st'
-  },
-  {
-    name: 'Example Restaurant 3',
-    addr: '123 North st'
-  }
+  {name: 'React Cafe', address: '123 Anywhere St'},
+  {name: 'Fancy Restaurant', address: '799 Main St'},
+  {name: 'Taco Place', address: '550 Maple Rd'},
+  {name: "Tony's Diner", address: '4101 College St'},
+  {name: 'Pasta Central', address: '706 Harper St'},
+  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
+  {name: 'Pizza Express', address: '1049 Bird St'},
+  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
+  {name: 'Maroon Deli', address: '1082 Stuart St'},
+  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
+  {name: 'Dumpling House', address: '747 Kelly Dr'},
+  {name: 'Hot Chicken', address: '1816 Olive St'},
+  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
+  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
+  {name: "Bobby's Burgers", address: '4152 Berkley St'},
+  {name: 'Turnpike Diner', address: '4571 Central Ave'},
+  {name: 'Bombay Express', address: '65 Queens Lane'},
+  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
+  {name: "King's Garden", address: '2935 Victoria Ct'},
+  {name: 'Salads and More', address: '2454 Preston St'},
 ]
 
 export default class App extends Component {
@@ -27,18 +35,8 @@ export default class App extends Component {
   }
   render() {
 
-    //This logs to chrome console
     console.log('this.state.searchVal')
     console.log(this.state.searchVal)
-
-    /*
-      THIS puts the 'warning' on the ios simulator
-      AND yellow box in chrome console
-      console.warn('text')
-
-      THIS puts a big red error screen
-      console.error('text')
-    */
     
 
     let placesInList = places.filter(place => {
--- GithubFetcher/index.js
+++ GithubFetcher/index.js
@@ -6,4 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//otpional to disable yellow box in simulator
+// console.disableYellowBox = true
+
 AppRegistry.registerComponent(appName, () => App);
+
